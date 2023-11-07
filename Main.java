591800f7c1acaaaf6b8ea51036d8c05055b75501
Radiatore2 <148353306+Radiatore2@users.noreply.github.com>
2023-11-07 16:43:55 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main (String[] args) {
-
-        Animale animale = new Animale();
-        Gatto gatto = new Gatto();
-         System.out.println("L'animale dice " + animale);
-         System.out.print("Il gatto dice " + gatto);
-    }
-}
