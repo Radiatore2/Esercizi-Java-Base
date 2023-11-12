5b6142fefb94f2496df62aaec8052589def96b8a
Radiatore2 <radiatore2@gmail.com>
2023-11-12 16:29:35 +0100

    Esercizio 1 StringBuilder (Parte 3)

--- EX1STRBUILD/src/EX1STRBUILD.java
+++ EX1STRBUILD/src/EX1STRBUILD.java
@@ -3,7 +3,7 @@ public class EX1STRBUILD {
         String x = "Bellissimo";
         String y = "Bruttissimo";
 
-        if (x.equals(y)){
+        if (x.compareTo(y) == 0){
             System.out.println("Le due stringhe sono uguali");
         } else {
             System.out.println("Le due stringhe sono differenti");
