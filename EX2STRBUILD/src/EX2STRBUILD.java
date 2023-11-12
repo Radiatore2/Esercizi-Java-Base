f9837a20d5ae6a710317c182506fb78299dfa700
Radiatore2 <radiatore2@gmail.com>
2023-11-12 17:42:06 +0100

    Esercizio 2 StringBuilder (Parte 3)

--- EX2STRBUILD/src/EX2STRBUILD.java
+++ EX2STRBUILD/src/EX2STRBUILD.java
@@ -1,4 +1,20 @@
 public class EX2STRBUILD {
 
-    public static String uniCode ()
+    public static String uniCode (int index, String input) {
+
+        StringBuilder x = new StringBuilder();
+
+        if  (index<input.length()){
+            x.append(input.charAt(index));
+        } else {
+            x.append("L'indice è superiore alla stringa");
+        }
+
+        return x.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(uniCode(2, "Ciaone"));
+    }
 }
